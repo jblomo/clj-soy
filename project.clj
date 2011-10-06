3973d6b650c9e7f9c111ebe0c69df69584c296a9
Jim Blomo <jblomo@yelp.com>
2011-10-05 22:27:45 -0700

    1.3.0 release

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-soy "0.3.0-jblomo"
+(defproject clj-soy "0.3.0"
   :description "A Clojure interface to the Google Closure templating library"
-  :dependencies [[org.clojure/clojure "1.3.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-soy/google-closure-templates "20100708"]]
   :dev-dependencies [[lein-clojars "0.6.0"]
                      [swank-clojure "1.2.1"]])
